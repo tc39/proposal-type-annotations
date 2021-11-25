671a023877656fecda85e7c6cb32ef6f64224d16
Romulo Cintra <romulocintra@users.noreply.github.com>
2021-11-25 12:34:56 +0100

    Update site.jsx

--- site/src/site.jsx
+++ site/src/site.jsx
@@ -8,6 +8,7 @@ const Page = () => <html lang="en">
     <head>
         <meta charSet="utf-8"/>
         <title>TC39 Proposal: Types as Comments</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="Reserve a space for static type syntax inside the ECMAScript language. JavaScript engines would treat type syntax as comments." />
         <meta property="og:title" content="TC39 Proposal: Types as Comments" />
         <meta property="og:type" content="article" />
